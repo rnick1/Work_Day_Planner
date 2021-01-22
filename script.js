b5b2a62724fa6e93b4ee3fa51967e32b0a6e5d0b
rnick1 <rnick0260@gmail.com>
2021-01-22 14:36:59 -0800

    Added initial script.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+var currentDay = document.getElementById("currentDay");
+currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+
 // To do:
+
+// Add date/time to currentDay id
+// Make elapsed hours automatically grey-out
+// Make the current hour red
+// Make upcoming hours green
+// Weekends must not be included
+// Add event handlers to the submit buttons. These will save the entry on local storage.
 // 
 
 
