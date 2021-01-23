bf51adc3e0d61c16977a77e71c880eabc4420436
rnick1 <rnick0260@gmail.com>
2021-01-23 15:04:07 -0800

    Updates.

---
+++ notes_script.js
@@ -0,0 +1,34 @@
+// $(document).ready(function() {
+
+    // var table = document.getElementById("table");
+    // var time = document.getElementsByClassName("time-slot");
+    var calendarItem = document.getElementsByClassName("form-control");
+    // var addItem = document.getElementsByClassName("add-item-button");
+
+    var currentDay = new Date();
+    document.getElementById("currentDay").innerHTML = currentDay;
+
+var calendarItem = "";
+
+$(".add-item-button").on("click", function() {
+    $("#second-number").text(secondNumber);
+});
+
+
+
+
+// });
+// To do:
+
+// Make elapsed hours automatically grey-out
+// Make the current hour red
+// Make upcoming hours green
+// Weekends must not be included
+// Add event handlers to the submit buttons. These will save the entry on local storage.
+// 
+// Attach event handlers to the save button class
+// Run an alert if the user clicks save an no text has been entered
+// Have the text save to local storage 
+// Then have the text display in place of the text box
+
+
