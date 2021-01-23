912efaa3425ca0d220a1b7f03c39b46f788acf08
rnick1 <rnick0260@gmail.com>
2021-01-23 14:33:21 -0800

    Fixed issue regarding moment.js.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // $(document).ready(function() {
 
-    var table = document.getElementById("table");
-    var time = document.getElementsByClassName("time-slot");
+    // var table = document.getElementById("table");
+    // var time = document.getElementsByClassName("time-slot");
     var calendarItem = document.getElementsByClassName("form-control");
-    var addItem = document.getElementsByClassName("add-item-button");
+    // var addItem = document.getElementsByClassName("add-item-button");
+
     var currentDay = new Date();
     document.getElementById("currentDay").innerHTML = currentDay;
 
+var calendarItem = "";
+
+$(".add-item-button").on("click", function() {
+    $("#second-number").text(secondNumber);
+    if
+});
 // });
 // To do:
 
@@ -16,5 +23,9 @@
 // Weekends must not be included
 // Add event handlers to the submit buttons. These will save the entry on local storage.
 // 
+// Attach event handlers to the save button class
+// Run an alert if the user clicks save an no text has been entered
+// Have the text save to local storage 
+// Then have the text display in place of the text box
 
 
