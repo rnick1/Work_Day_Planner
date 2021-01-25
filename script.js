9e2433337b08839e4f94bf2ca30c1439ce6be7e9
rnick1 <rnick0260@gmail.com>
2021-01-25 11:13:38 -0800

    Added timeSlot and added spacing.

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
 $(document).ready(function() {
 
     // Date and time that updates thanks to moment.js:
-    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
+    $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm a'));
 
-    // Below is supposed to take what the user puts into the textarea and put it into local storage. After it is in local storage, 
-    // it is supposed to replace the text area with a para tag with the content in it.
-    var textArea = document.querySelector(".textarea");
+    // Below is supposed to take what the user puts into the textarea and put it into local storage. 
+    var textArea = document.querySelector('.textarea');
     var savedText = [];
-    var localStorageContent = localStorage.getItem("savedText");
+    var localStorageContent = localStorage.getItem('savedText');
 
     if (localStorageContent !==null) {
         savedText = JSON.parse(localStorageContent); 
-    }
+    };
 
-    $(".saveBtn").on('click', function(event) {
+    $('.saveBtn').on('click', function(event) {
+        
         var text = $(this).parent().siblings('td').children('textarea').val();
         console.log(text);
+        
+        var timeSlot = $(this).parent().siblings('td').children('hour').val();
+        console.log(timeSlot);
+        
         var userInput = {
             Text: text,
+            Time: timeSlot, 
         };
+
         savedText.push(userInput);
         event.preventDefault();
-        localStorage.setItem("savedText", JSON.stringify(savedText));
+        localStorage.setItem('savedText', JSON.stringify(savedText));
         displayText();
     });
 
     function displayText() {
-        textArea.setAttribute('style', "display: none");
-        for (var i = 0; i < savedText.length; i++);
+        textArea.setAttribute('style', 'display: none');
+        for (var i = 0; i < savedText.length; i++) {
             var textItem = savedText[i].Text;
+            var textTime = savedText[i].Time;
             var scheduleItem = document.createElement('p');
             scheduleItem.textContent = textItem;
-            textArea.append(scheduledItem);
+            // textArea.append(scheduleItem);
         };
+    }
 });
