d5c29de735cafbfc1af22c8543d73ab4c9bfe49d
rnick1 <rnick0260@gmail.com>
2021-01-25 15:20:44 -0800

    Removed all javascript...starting over.

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
     console.log(savedText);
     var localStorageContent = localStorage.getItem('savedText');
 
+
     if (localStorageContent !== null) {
         savedText = JSON.parse(localStorageContent); 
     };
@@ -16,11 +17,26 @@ $(document).ready(function() {
     $('.saveBtn').on('click', function(event) {
         var text = $(this).parent().siblings('td').children('textarea').val();
         console.log(text);
-        var timeSlot = $(this).parent().siblings('th').text();
-        console.log(timeSlot);
+        var Text9 = document.getElementById('9text');
+        var Text10 = document.getElementById('10text');
+        var Text11 = document.getElementById('11text');
+        var Text12 = document.getElementById('12text');
+        var Text1 = document.getElementById('1text');
+        var Text2 = document.getElementById('2text');
+        var Text3 = document.getElementById('3text');
+        var Text4 = document.getElementById('4text');
+        var Text5 = document.getElementById('5text');
         var userInput = {
             Text: text,
-            Time: timeSlot, 
+            Time9: Text9,
+            Time10: Text10, 
+            Time11: Text11, 
+            Time12: Text12, 
+            Time1: Text1, 
+            Time2: Text2, 
+            Time3: Text3, 
+            Time4: Text4, 
+            Time5: Text5, 
         };
         console.log(userInput);
         savedText.push(userInput);
@@ -31,12 +47,12 @@ $(document).ready(function() {
 
     function displayText() {
         for (var i = 0; i < savedText.length; i++) {
-            var textItem = savedText[i].Text;
-            console.log(textItem);
+            // var textItem = savedText[i].Text;
+            // console.log(textItem);
             
-            var storedText = localStorage.getItem('Text');
+            // var storedText = localStorage.getItem('Text');
 
-            textArea = textContent.Text
+            // textArea = textContent.Text
         };
-    }
+    };
 });
