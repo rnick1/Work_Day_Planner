2c165e0ccc66e16b0c335183eb4df51710e87fd3
rnick1 <rnick0260@gmail.com>
2021-01-24 17:07:20 -0800

    update.

--- notes_script.js
+++ notes_script.js
@@ -1,21 +1,39 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
-    // var table = document.getElementById("table");
-    // var time = document.getElementsByClassName("time-slot");
-    var calendarItem = document.getElementsByClassName("form-control");
-    // var addItem = document.getElementsByClassName("add-item-button");
+    // Date and time that updates thanks to moment.js:
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
 
-    var currentDay = new Date();
-    document.getElementById("currentDay").innerHTML = currentDay;
+    // Below is supposed to take what the user puts into the textarea and put it into local storage. After it is in local storage, 
+    // it is supposed to replace the text area with a para tag with the content in it.
+    var textArea = document.querySelector(".textarea");
+    var savedText = [];
+    var localStorageContent = localStorage.getItem("savedText");
 
-var calendarItem = "";
+    if (localStorageContent !==null) {
+        savedText = JSON.parse(localStorageContent); 
+    }
 
-$(".add-item-button").on("click", function() {
-    $("#second-number").text(secondNumber);
+    $(".saveBtn").on('click', function(event) {
+        var text = $(this).parent().siblings('td').children('textarea').val();
+        console.log(text);
+        var userInput = {
+            Text: text,
+        };
+        savedText.push(userInput);
+        event.preventDefault();
+        localStorage.setItem("savedText", JSON.stringify(savedText));
+        displayText();
+    });
+
+    function displayText() {
+        textArea.setAttribute('style', "display: none");
+        for (var i = 0; i < savedText.length; i++);
+            var textItem = savedText[i].Text;
+            var scheduleItem = document.createElement('p');
+            scheduleItem.textContent = textItem;
+            textArea.append(scheduledItem);
+        };
 });
-
-
-
 
 // });
 // To do:
--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 
+    // Date and time that updates thanks to moment.js:
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
 
-    var textArea = document.querySelector("#textarea");
+    // Below is supposed to take what the user puts into the textarea and put it into local storage. After it is in local storage, 
+    // it is supposed to replace the text area with a para tag with the content in it.
+    var textArea = document.querySelector(".textarea");
     var savedText = [];
     var localStorageContent = localStorage.getItem("savedText");
 
@@ -10,9 +13,11 @@ $(document).ready(function() {
         savedText = JSON.parse(localStorageContent); 
     }
 
-    $("#saveBtn").on('click', function(event) {
+    $(".saveBtn").on('click', function(event) {
+        var text = $(this).parent().siblings('td').children('textarea').val();
+        console.log(text);
         var userInput = {
-            Text: textArea.value,
+            Text: text,
         };
         savedText.push(userInput);
         event.preventDefault();
