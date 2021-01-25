93af74dbd1cf454e3459e67e132db4b2d00a070e
rnick1 <rnick0260@gmail.com>
2021-01-24 16:09:32 -0800

    Added code for local storage.

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
 
-    $("#currentDay").text(moment().format('DDD-MMMM-YYYY'));
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
 
-})
+    var textArea = document.querySelector("#textarea");
+    var savedText = [];
+    var localStorageContent = localStorage.getItem("savedText");
+
+    if (localStorageContent !==null) {
+        savedText = JSON.parse(localStorageContent); 
+    }
+
+    $("#saveBtn").on('click', function(event) {
+        var userInput = {
+            Text: textArea.value,
+        };
+        savedText.push(userInput);
+        event.preventDefault();
+        localStorage.setItem("savedText", JSON.stringify(savedText));
+        displayText();
+    });
+
+    function displayText() {
+        textArea.setAttribute('style', "display: none");
+        for (var i = 0; i < savedText.length; i++);
+            var textItem = savedText[i].Text;
+            var scheduleItem = document.createElement('p');
+            scheduleItem.textContent = textItem;
+            textArea.append(scheduledItem);
+        };
+});
