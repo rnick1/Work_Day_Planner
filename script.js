76efde6a564c6685c2492d9747aa574fcff85d71
rnick1 <rnick0260@gmail.com>
2021-01-26 10:47:59 -0800

    Reduced the local storage problem to just the essentials.

--- script.js
+++ script.js
@@ -8,38 +8,34 @@ $(document).ready(function() {
     var savedText = [];
     var localStorageContent = localStorage.getItem('savedText');
 
-    if (localStorageContent !== null) {
-        savedText = JSON.parse(localStorageContent); 
-    };
-    console.log(localStorageContent);
+    textArea.textContent = JSON.parse(localStorageContent);
+
+    // if (localStorageContent !== null) {
+    //     savedText = JSON.parse(localStorageContent); 
+    // };
+    // console.log(localStorageContent);
 
     $('.saveBtn').on('click', function(event) {
         var text = $(this).parent().siblings('td').children('textarea').val();
-        console.log(text);
-        var timeSlot = $(this).parent().siblings('th').text();
-        console.log(timeSlot);
-        var userInput = {
-            Text: text,
-            Time: timeSlot, 
-        };
-        console.log(userInput);
+        // console.log(text);
+        // var timeSlot = $(this).parent().siblings('th').text();
+        // console.log(timeSlot);
+        var userInput = text;
+            // Time: timeSlot, 
+
+        // console.log(userInput);
         savedText.push(userInput);
         event.preventDefault();
         localStorage.setItem('savedText', JSON.stringify(savedText));
     });
 
 // Everything below this is supposed to take it out of local storage and display it in the text areas.
-    function displayText() {
-        for (var i = 0; i < savedText.length; i++) {
-            var textItem = savedText[i].Text;
-            console.log(textItem);
-            $("#timetable").append("<th>");
-            $("#timetable").append("Hello world");
-            $("#timetable").append("</th>");
+//     function displayText() {
+//         for (var i = 0; i < savedText.length; i++) {
+//             var textArea = savedText[i].Text;
+//         };
+//     }
 
-        };
-    }
-
-displayText();
+// displayText();
 
 });
